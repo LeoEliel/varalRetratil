e623a51050d0d292d52e5d342378ab350e312d4f
LeoEliel <39065757+LeoEliel@users.noreply.github.com>
2019-09-20 17:12:19 -0400

    Usando cURL para recuperar JSON da API Advsor

--- main.php
+++ main.php
@@ -1,6 +1,24 @@
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url);
-$result=curl_exec($ch);
-curl_close($ch);
-var_dump(json_decode($result, true));
+<?php
+    //url
+    $url="http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/5757/days/15?token=fcc72a6c209aa5e8b8d1e1954c00a0fd";
+    //  Initiate curl
+
+    $ch = curl_init();
+
+    // Will return the response, if false it print the response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Set the url
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    // Execute
+    $result=curl_exec($ch);
+
+
+    
+    // Closing
+    curl_close($ch);
+
+    // Will dump a beauty json :3
+    $json = json_decode($result, true);
+?>
