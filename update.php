7345946433ddd70e47ce918f5ac97505b6a02852
LeoEliel <39065757+LeoEliel@users.noreply.github.com>
2019-09-26 12:23:47 -0400

    CrUd prontos.

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 // Include config file
 require_once "config.php";
- 
+require_once "json.php";
 
 // Define variables and initialize with empty values
     $dataPrevisao = $humiMax = $humiMin = $chuvaProbab = "";
