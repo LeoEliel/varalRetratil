98a9f145bf3ea5f21e30014676e23b0b527bb4f5
LeoEliel <39065757+LeoEliel@users.noreply.github.com>
2019-10-04 15:45:36 -0400

    consertando delete.

--- delete.php
+++ delete.php
@@ -2,27 +2,17 @@
 // Include config file
 require_once "config.php";
 
-
-// $id = 0;
-
 // Prepare a delete statement
-$sql = "DELETE FROM previsao"; //  WHERE id = :id";
-// Bind variables to the prepared statement as parameters
-// $stmt->bindParam(":id", $param_id);
-// Set parameters
-//$param_id = $id;
+$sql = "DELETE FROM previsao";
 
-    if ($stmt = pdo->query(sql)) {
+
+    if ($stmt = $pdo->exec($sql)) {
         echo "<p> Registros apagados com sucesso, redirecionando para o Dashboard</p>";
         header("location: index.php");
         exit();
     } else {
         echo "<p>Opa! Algo não funcionou como devia. Tente novamente mais tarde.</p>";
     }
-
-    // Close statement
-    unset($stmt);
-
-    // Close connection
-    unset($pdo);
+// Close connection
+unset($pdo);
 ?>
