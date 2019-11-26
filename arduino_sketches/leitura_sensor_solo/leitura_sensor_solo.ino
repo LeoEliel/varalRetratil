bce2e315ad29b395b620ce3f9020fa1c6ac8cd6d
LeoEliel <39065757+LeoEliel@users.noreply.github.com>
2019-11-26 16:57:45 -0400

    terminando código arduino ~ python com a ponte H.

--- arduino_sketches/leitura_sensor_solo/leitura_sensor_solo.ino
+++ arduino_sketches/leitura_sensor_solo/leitura_sensor_solo.ino
@@ -1,28 +1,49 @@
 //Programa: Monitoracao de planta usando Arduino
 //Autor: FILIPEFLOP
 // Modificado por Leonardo Eliel Dias da Silva
- 
-#define pino_sinal_analogico A0
- 
+
+#define sinal_sensor A0
+int IN1 = 4;
+int IN2 = 5;
+int LED = 13;
 int valor_analogico;
- 
+
 void setup()
-{
+{  
+  pinMode(sinal_sensor, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
   Serial.begin(9600);
-  pinMode(pino_sinal_analogico, INPUT);
-  Serial.print("Teste Sensor de Umidade!");
 }
- 
+
 void loop()
-
-
-{
-  //Le o valor do pino A0 do sensor
-  valor_analogico = analogRead(pino_sinal_analogico);
- 
-  //Mostra o valor da porta analogica no serial monitor
-  Serial.print('\n');
-  Serial.print("Leitura da Porta analogica: ");
-  Serial.print(valor_analogico);
+{  
+  valor_analogico = analogRead(sinal_sensor);
+    
+  Serial.println(valor_analogico);
+  if (Serial.available()) {
+    
+    char python = Serial.read();
+    
+    if (python == 'L') {      
+      digitalWrite(LED, HIGH);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);      
+      delay(5000);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(LED, LOW);            
+    }
+    if (python == 'D') {      
+        digitalWrite(LED, HIGH);
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);      
+        delay(5000);
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(LED, LOW);            
+      }   
+  }
   delay(1000);
 }
--- py/config.py
+++ py/config.py
@@ -2,31 +2,41 @@
 # -*- coding: utf-8 -*-
 import pymysql
 import serial
+import time
 from serial.tools import list_ports
 
-conexao = pymysql.connect(db='1iot', user='root', passwd='')#formando conexão com banco de dados. Insira seus parâmetros correspondentes
-cursor = conexao.cursor()
-cursor.execute("select * from previsao")#SQL Query aqui
-resultado = cursor.fetchall()#Retorno do Resultado. Observar sobre os comandos cursor.fetchone e cursor.fetchall
-print("previsao:") #apenas um cabeçalho para printar na tela
-for info_bd in resultado: # laço que imprime na tela os resultados contidos na variavel "resultado"
-    print(info_bd)
-conexao.close()#fecha conexão do BD
+def lerProbabChuva():
+    conexao = pymysql.connect(db='1iot', user='root', passwd='')
+    cursor = conexao.cursor()
+    cursor.execute("select * from previsao")
+    resultado = cursor.fetchall()
+    print("previsao:") 
+    for info_bd in resultado: 
+        print(info_bd)
+    conexao.close()
+    return info_bd[4]
+
+def lerSerial():    
+    info_sensor = serial.readline()
+    print("Serial: \n",info_sensor)      
+    return info_sensor    
+
+def ligarMotor():        
+    print("\n")        
+    humidSensor = lerSerial()
+    probabChuva = lerProbabChuva()
+    if probabChuva >=50 and humidSensor >= b'512\r\n':        
+        serial.write(bytes("L",'utf-8'))
+        print ("Chuva detectada! Sinal p/ LED e Motor: ATIVAR")  
+        time.sleep(1800)
+        humidSensor = lerSerial()
+        if humidSensor < b'512\r\n':
+            serial.write(bytes("D",'utf-8'))
+            print ("Chuva não detectada! Sinal p/ LED e Motor: DESATIVAR")  
+    else:
+        print("Chuva não detectada. Sinal p/ LED e Motor: NONE")            
+        time.sleep(5)
+    ligarMotor()
 
 serial = serial.Serial('COM3', 9600)
-
-try: # Cada execução lê uma linha da porta serial e separada pela voltagem so solo coletada pelo sensor imprimindo no prompt
-    for linha in serial:
-        try:
-            entrada = linha.decode("utf-8").split("\t")
-            info_sensor = int(entrada[0])
-            print(u"Voltagem do Solo: %g"%(info_sensor)%" Volts")
-        except ValueError as e:
-            print("E: %s"%linha)
-        except IndexError as e:
-            print("E: %s"%linha)
-except KeyboardInterrupt: # Ao abortar a execução do programa esta exception é chamada deve-se então fechar a porta serial para novas comunicações
-    serial.close()
-except: #Caso seja um erro não especificado é importante fechar a porta serial para permitir comunicação futura
-    serial.close()
-
+ligarMotor()
